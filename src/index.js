70438ba16dc7fd113702eb15123dbd00dc8db8c1
Juan Ignacio Casal <ignaciocasal@gmail.com>
2017-03-06 11:30:21 -0300

    Adding Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+
+import Root from './containers/root'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root')
 );
